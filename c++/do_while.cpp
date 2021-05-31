798d3390f2ae335128289d2955e2ec6620a6e575
chin2 <maniprema972@gmail.com>
2021-05-31 12:49:38 +0530

    31_05_2012

---
+++ c++/do_while.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int i=0;
+    do{
+        cout<<"Hello world "<<i+1<<endl;
+        i++;
+    }while(i<5);
+return 1;
+}
---
+++ c++/while.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int i=0;
+    while(i<5){
+        cout<<"Hello world "<<i+1<<endl;
+        i++;
+    }
+return 1;
+}
